6c594b3b48ad169b4db0c8b6c7527c085ae9276c
sebaek <sebaek@gmail.com>
2024-05-21 14:49:28 +0900

    한 게시물 보기

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Home } from "./page/Home.jsx";
 import { BoardWrite } from "./page/board/BoardWrite.jsx";
 import { BoardList } from "./page/board/BoardList.jsx";
+import { BoardView } from "./page/board/BoardView.jsx";
 
 const router = createBrowserRouter([
   {
@@ -15,6 +16,7 @@ const router = createBrowserRouter([
         element: <BoardList />,
       },
       { path: "write", element: <BoardWrite /> },
+      { path: "board/:id", element: <BoardView /> },
     ],
   },
 ]);
