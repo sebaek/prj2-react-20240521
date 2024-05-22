eacf3a54be5a6b86bb89e7123d063164db230ca8
sebaek <sebaek@gmail.com>
2024-05-22 09:19:45 +0900

    회원가입화면
    네브바에 경로 추가

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -25,6 +25,15 @@ export function Navbar() {
       >
         글쓰기
       </Box>
+      <Box
+        onClick={() => navigate("/signup")}
+        cursor={"pointer"}
+        _hover={{
+          bgColor: "gray.200",
+        }}
+      >
+        회원가입
+      </Box>
     </Flex>
   );
 }
