fd66852b90d64cc409b8f633f945c3e56919c294
sebaek <sebaek@gmail.com>
2024-05-23 14:26:46 +0900

    로그인 처리

--- src/page/member/MemberLogin.jsx
+++ src/page/member/MemberLogin.jsx
@@ -1,6 +1,29 @@
-import { Box, Button, FormControl, FormLabel, Input } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  useToast,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export function MemberLogin() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const toast = useToast();
+  const navigate = useNavigate();
+
+  function handleLogin() {
+    axios
+      .post("/api/member/token", { email, password })
+      .then((res) => {})
+      .catch(() => {})
+      .finally(() => {});
+  }
+
   return (
     <Box>
       <Box>로그인</Box>
@@ -8,17 +31,19 @@ export function MemberLogin() {
         <Box>
           <FormControl>
             <FormLabel>이메일</FormLabel>
-            <Input />
+            <Input onChange={(e) => setEmail(e.target.value)} />
           </FormControl>
         </Box>
         <Box>
           <FormControl>
             <FormLabel>암호</FormLabel>
-            <Input />
+            <Input onChange={(e) => setPassword(e.target.value)} />
           </FormControl>
         </Box>
         <Box>
-          <Button colorScheme={"blue"}>로그인</Button>
+          <Button onClick={handleLogin} colorScheme={"blue"}>
+            로그인
+          </Button>
         </Box>
       </Box>
     </Box>
